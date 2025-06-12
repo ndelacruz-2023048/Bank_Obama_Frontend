8379d64f197bf08aa9bc9e276092accb75c5c3e8
jgramajo-2023053 <jgramajo-2023053@kinal.edu.gt>
2025-06-12 14:23:07 -0600

    Creacion del diseño de ProfileTemplade, modificacion del SideBar

--- src/components/organimos/sidebar/index.jsx
+++ src/components/organimos/sidebar/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { useLogout } from '../../../hooks/useLogout'
+import { useNavigate } from 'react-router'
 
 export const Sidebar = () => {
   const { logout } = useLogout()
+  const navigate = useNavigate()
 
   const handleLogout = () => {
     logout()
+  }
+
+  const goProfile = () => {
+    navigate('/profile')
   }
 
   return (
@@ -14,6 +20,11 @@ export const Sidebar = () => {
       <div onClick={handleLogout} >
         <button>
           Cerrar sesión
+        </button>
+      </div>
+      <div onClick={goProfile} >
+        <button>
+          Profile
         </button>
       </div>
     </div>
--- src/components/template/ProfileTemplate.jsx
+++ src/components/template/ProfileTemplate.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export const ProfileTemplate = () => {
   return (
-    <div>ProfileTemplate</div>
+    <div className="min-h-screen w-[80vw] bg-[#121721] text-white flex justify-center items-start p-6">
+      <div className="w-full max-w-2xl p-8 rounded-lg">
+        <h2 className="text-2xl font-semibold mb-6">Información personal</h2>
+
+        <div className="mb-6">
+          <h3 className="font-semibold mb-4">Información de contacto</h3>
+          <div className="space-y-4">
+            <input type="text" placeholder="Nombre" className="w-full px-4 py-2 bg-[#192633] rounded focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-[15px]" />
+            <input type="text" placeholder="Dirección" className="w-full px-4 py-2 bg-[#192633] rounded focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-[15px]" />
+            <input type="text" placeholder="Nombre de Trabajo" className="w-full px-4 py-2 bg-[#192633] rounded focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-[15px]" />
+            <input type="number" placeholder="Ingresos Mensauales" className="w-full px-4 py-2 bg-[#192633] rounded focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-[15px]" />
+          </div>
+        </div>
+
+        <div className="text-right">
+          <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded">
+            Guardar cambios
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
